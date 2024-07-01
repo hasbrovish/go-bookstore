0b862e68ff91bb4c75f8919e299f272b9e20958c
hasbrovish <hasbrovish95@gmail.com>
2024-07-01 12:16:54 +0530

    added utils and router

--- pkg/routes/book-routes.go
+++ pkg/routes/book-routes.go
@@ -7,4 +7,8 @@ import (
 
 var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBooks).Methods("GET")
+	router.HandleFunc("/book/{bookid}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookid}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookid}", controllers.DeleteBook).Methods("DELETE")
 }
---
+++ pkg/utils/utils.go
@@ -0,0 +1,16 @@
+// Util : request from user --> to unmarshal or decode data
+package utils
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func ParseBody(r *http.Request, x interface{}) {
+	if body, err := io.ReadAll(r.Body); err == nil {
+		if err := json.Unmarshal([]byte(body), x); err != nil {
+			return
+		}
+	}
+}
